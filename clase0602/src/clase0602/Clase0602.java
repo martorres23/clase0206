87706212522fc64010c8de456f90bacd35ce380e
martorres23 <patricia25tg@gmail.com>
2021-06-02 14:55:07 -0500

    versión 1

---
+++ clase0602/src/clase0602/Clase0602.java
@@ -0,0 +1,12 @@
+package clase0602;
+
+public class Clase0602 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Versión 0");
+    }
+    
+}
